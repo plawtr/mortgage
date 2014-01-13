5e5106a8e13d6f7b26633b791d293c548a9bc8d7
Andrey Krahmal <coolrighthere@yahoo.com>
2014-01-13 14:33:08 +0000

    Mortgage Deposit varies with the value of property.

--- mortgage.rb
+++ mortgage.rb
@@ -1,6 +1,6 @@
 def provide_mortgage?(salary, deposit, property_value)
   loan_amount = property_value - deposit
-  min_deposit = 0.2 # 20%
+  property_value <= 650000 ? min_deposit = 0.05 : min_deposit = 0.20 # 20%
   max_multiplier = 5 # how many annual incomes can be borrowed
   deposit >= property_value * min_deposit && 
       salary * max_multiplier >= loan_amount
