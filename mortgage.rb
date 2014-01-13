c7df22cab438cf59cbc0498d64a1660cab783d41
Andrey Krahmal <coolrighthere@yahoo.com>
2014-01-13 14:36:06 +0000

    Waive the limite on the loan amount if deposit more than 75%

--- mortgage.rb
+++ mortgage.rb
@@ -2,6 +2,7 @@ def provide_mortgage?(salary, deposit, property_value)
   loan_amount = property_value - deposit
   property_value <= 650000 ? min_deposit = 0.05 : min_deposit = 0.20 # 20%
   max_multiplier = 5 # how many annual incomes can be borrowed
+  return true if deposit/property_value >= 0.75
   deposit >= property_value * min_deposit && 
       salary * max_multiplier >= loan_amount
 end
